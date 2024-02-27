371eb0719a15cc05e2bcb0aec73cd9b17f009fee
kiyotaka <c4mo.sw@gmail.com>
2024-02-27 12:35:45 -0500

    feat(extend): Extending colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  
- content: [
-
-  "./index.html",
-
-  "./src/**/*.{js,ts,jsx,tsx}",
-
-],
-
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'leveled': {
+          '50': '#e3b5b5',
+          '100': '#dcacba',
+          '200': '#d28eae',
+          '300': '#c261a1',
+          '400': '#9d3f8d',
+          '500': '#6f2f6f',
+          '600': '#4b1e52',
+          '700': '#2d1537',
+          '800': '#1a0f24',
+          '900': '#110b18',
+          '950': '#040308',
+        },    
+      }
+    },
   },
   plugins: [],
-}
-
+};
