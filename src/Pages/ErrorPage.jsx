a1c1dabb94016affc2148f4ae8176b5d247636a6
kiyotaka <c4mo.sw@gmail.com>
2024-02-27 12:36:01 -0500

    feat(Pages): Ading all pages

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen flex justify-center items-center">ErrorPage</div>
+    <div>ErrorPage</div>
   )
 }
 
--- src/Pages/GamePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GamePage = () => {
-  return (
-    <div className="h-screen flex justify-center items-center">GamePage</div>
-  );
-};
-
-export default GamePage;
