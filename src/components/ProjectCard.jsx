2fa80186484c9d995fcbb24dec3f9576beaf87ac
kiyotaka <c4mo.sw@gmail.com>
2024-02-28 23:23:56 -0500

    feat(ProjectCard): Project card is created

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const ProjectCard = ({ data }) => {
   const { name, image, description, tags, source_code_link, deploy_link } =
     data;
+
+  const card = useRef(null);
+  const [leave, setLeave] = useState(false)
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
 
   const [resolvedImage, setResolvedImage] = useState(null);
 
@@ -23,20 +28,61 @@ const ProjectCard = ({ data }) => {
     loadImage();
   }, []);
 
+
+  useEffect(() => {
+    const rotateElement = (e) => {
+      setLeave(false)
+      const cardRect = card.current.getBoundingClientRect();
+      
+      const centerX = cardRect.width / 2;
+      const centerY = cardRect.height / 2;
+      
+      const offsetX = e.clientX - cardRect.left;
+      const offsetY = e.clientY - cardRect.top;
+      
+      const rotateX = ((centerY - offsetY) / centerY) * 6;
+      setRotateX(rotateX);
+      const rotateY = ((offsetX - centerX) / centerX) * 6;
+      setRotateY(rotateY);
+    };
+    
+    const onLeave = () => {
+      setLeave(true)
+      setRotateX(0)
+      setRotateY(0)
+    }
+    
+    card.current.addEventListener("mousemove", rotateElement);
+    card.current.addEventListener("mouseleave", onLeave);
+    
+    return () => {
+      if (!card.current) return
+      card.current.removeEventListener("mousemove", rotateElement);
+      card.current.removeEventListener("mouseleave", onLeave);
+    };
+  }, [card.current]);
+  
   return (
     // Card background
     <div
-      style={{
+      ref={card}
+      style={leave ? {
         transformStyle: "preserve-3d",
+        transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`, 
+        transition: 'transform 0.3s ease'   
+      } : {
+        transformStyle: "preserve-3d",
+        transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,   
       }}
-      className="w-full lg:w-5/12 bg-gradient-to-br from-blue-300 dark:from-leveled-400 to-blue-600 transition-colors duration-500 ease-out dark:to-leveled-600 to-50% p-4 m-2 rounded-2xl"
+      className="w-full lg:w-[40%] bg-gradient-to-br from-cyan-300 dark:from-leveled-400 to-cyan-700 dark:to-leveled-600 transition-colors duration-500 ease-out  to-50% p-6 mr-6 rounded-2xl shadow-2xl shadow-neon dark:shadow-leveled-200 h-auto"
     >
       {/* Cristal */}
       <div
         style={{
           transformStyle: "preserve-3d",
+          transform: "translateZ(100px)",
         }}
-        className="inset-4 place-content-center bg-slate-500 rounded-xl shadow-lg shadow-emerald-400 p-3 bg-opacity-60"
+        className="inset-4 place-content-center bg-leveled-500 dark:bg-neon rounded-xl shadow-lg shadow-emerald-400 p-6 bg-opacity-50 dark:bg-opacity-20"
       >
         {/* Project image */}
         <div
@@ -44,25 +90,27 @@ const ProjectCard = ({ data }) => {
             transformStyle: "preserve-3d",
             transform: "translateZ(100px)",
           }}
-          className="relative w-full h-full shadow-2xl"
+          className="relative w-full h-full"
         >
           {resolvedImage ? (
             <img
               src={resolvedImage}
               alt={name}
-              className="w-full h-full object-center object-cover rounded-lg"
+              className="w-full h-[50%] object-center object-cover rounded-lg"
             />
           ) : null}
         </div>
 
-        <div className="">
-          <h3 className="py-4 tracking-wider">{name}</h3>
-          <p>
+        <div className="text-cyan-100">
+          <h3 className="text-xl py-4 tracking-wider font-bold light-neon-shadow dark:dark-neon-shadow">
+            {name}
+          </h3>
+          <p className="text-lg font-semibold">
             {description}{" "}
             {deploy_link ? (
               <Link
                 to={deploy_link}
-                className="hover:text-emerald-500 transition-colors duration-300"
+                className="text-leveled-200 hover:text-neon transition-colors duration-300 flex flex-col mt-5"
               >
                 {"Got to deployment"}
               </Link>
@@ -74,7 +122,7 @@ const ProjectCard = ({ data }) => {
           {tags.map((tag) => (
             <p
               key={tag.name}
-              className={`bg-clip-text text-transparent bg-gradient-to-br ${tag.color}`}
+              className={`bg-clip-text text-transparent bg-gradient-to-br ${tag.id}`}
             >
               #{tag.name}
             </p>
