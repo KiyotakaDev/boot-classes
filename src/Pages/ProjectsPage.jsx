8fbba0e72d406f3c41bb704ca89d3f2dff84e71b
kiyotaka <c4mo.sw@gmail.com>
2024-02-28 19:16:08 -0500

    feat(ProjectCard): Component is created

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ProjectCard from '../components/ProjectCard';
+import { projects } from '../constants';
 
 const ProjectsPage = () => {
   return (
-    <main>ProjectsPage</main>
-  )
+    <main>
+      <div className='w-full h-screen flex flex-col lg:flex-row justify-center items-center'>
+        {projects.map((project) => (
+          <ProjectCard key={project.name} data={project} />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default ProjectsPage
