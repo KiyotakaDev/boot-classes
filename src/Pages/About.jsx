6d908b913eb5358db3f39b74d59853d883e31b1c
kiyotaka <c4mo.sw@gmail.com>
2024-02-29 13:33:19 -0500

    feat(About): Adding about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React from "react";
+import { technologies } from "../constants";
+import TechTable from "../components/TechTable";
+import Experience_Education from "../components/Experience_Education";
 
 const About = () => {
   return (
-    <main className='text-4xl'>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam natus dignissimos nihil iusto, expedita amet vero totam dolor, eveniet veniam libero doloribus debitis sed commodi corporis facilis accusantium. Labore, voluptate.
-      Laboriosam voluptas quaerat sit cum harum et optio blanditiis vitae odit sed impedit debitis ipsum, perferendis dignissimos quis eum quo quam neque dolor? Voluptatum expedita ut soluta voluptas asperiores sequi?
-      Quis quasi iste dolores perferendis laboriosam nisi, numquam, sint ducimus voluptas dignissimos ipsa accusamus. Magni blanditiis doloribus voluptate ex at ullam dolor fuga harum quia minus! Eaque tempora nulla expedita.
-      Sunt itaque, assumenda, provident odio obcaecati aut minus neque ad ut pariatur saepe modi, ducimus ullam? Odit, mollitia ab officiis adipisci iusto fugit quia. Quis beatae praesentium repudiandae veniam magnam?
-      Accusamus eum voluptas mollitia nemo, explicabo, nam itaque tempora, necessitatibus esse veritatis ullam. Quam quo minima adipisci. Quae a magni ipsa aut at enim eum facilis minus minima! Cupiditate, commodi?
-      Accusamus expedita natus, repudiandae excepturi odio facilis autem ipsam ut vero minus corrupti error rem temporibus dolorem reiciendis ipsa at neque atque molestias earum dolor. Ipsum aliquid voluptatibus nulla ipsam?
-      Soluta eius distinctio beatae optio voluptatem repellendus accusamus, laboriosam consequuntur, obcaecati, doloribus autem aliquam! Voluptatem voluptate nisi recusandae repudiandae accusantium. Saepe maxime molestias voluptatum optio blanditiis provident nemo quia reiciendis!
-      Quasi vitae neque maxime nesciunt amet quaerat accusamus placeat eum libero exercitationem sed id similique vel, assumenda ipsum voluptatibus hic eaque quidem tempora architecto velit atque facilis. Pariatur, et ea!
-      Possimus exercitationem voluptas, minus distinctio quos doloribus ducimus labore, dolorem tenetur asperiores totam, ea assumenda nulla repellat odit. Deleniti, consectetur quas. Totam veniam autem iure dolorum id quis sed culpa?
-      Est facere aliquam beatae voluptas sint aspernatur omnis architecto mollitia libero, cupiditate maiores nesciunt, perspiciatis similique vero maxime! Incidunt excepturi voluptas aspernatur quos quibusdam ipsam sapiente architecto accusantium accusamus distinctio!
-    </main>
-  )
-}
+    <main className="relative h-screen">
+      <div className="w-[95%] absolute top-[5%]">
+        <h1 className="text-4xl font-bold mb-[4%]">About me</h1>
 
-export default About
+        <div className="mx-auto w-full p-4">
+          <div className="mb-8">
+            <h2 className="text-xl font-bold mb-2">Technologies and Tools</h2>
+            <div className="h-[15.5rem] overflow-y-scroll">
+              <table className="w-full table-auto">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-2">Thecnologies/Tools</th>
+                    <th className="px-4 py-2">Experience Level</th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {technologies.map((tech) => (
+                    <TechTable key={tech.name} tech={tech} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="h-[21rem] overflow-y-scroll text-2xl">
+            <div className="mb-8">
+              <Experience_Education />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default About;
--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ProjectCard from '../components/ProjectCard';
-import { projects } from '../constants';
+import React from "react";
+import ProjectCard from "../components/ProjectCard";
+import { projects } from "../constants";
 
 const ProjectsPage = () => {
   return (
-    <main>
-      <h2 className='font-bold text-4xl py-[4%]'>Projects</h2>
-      <div className='flex flex-col lg:flex-row mr-10'>
-        {projects.map((project) => (
-          <ProjectCard key={project.name} data={project} />
-        ))}
+    <main className="relative h-screen">
+      <div className="w-[95%] absolute top-[5%]">
+        <h1 className="text-4xl font-bold mb-[4%]">Projects</h1>
+
+        <div className="flex flex-col lg:flex-row mr-10">
+          {projects.map((project) => (
+            <ProjectCard key={project.name} data={project} />
+          ))}
+        </div>
       </div>
     </main>
   );
-}
+};
 
-export default ProjectsPage
+export default ProjectsPage;
