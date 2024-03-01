48a2a1bb39ab625db5acea360d493c80d3f5e986
kiyotaka <c4mo.sw@gmail.com>
2024-03-01 13:16:42 -0500

    feat(HomeIcon): Scale fixed

--- src/components/nav.svgs/Home.jsx
+++ src/components/nav.svgs/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = ({ text="tooltip" }) => {
   return (
     <div className='sidebar-icon group'>
-      <img src="/home.jpg" alt="Home" className='h-16 w-16 rounded-full p-2' />
+      <img src="/home.jpg" alt="Home" className='h-16 sm:h-24 lg:h-28 w-16 sm:w-24 lg:w-28 rounded-full p-2' />
       <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
     </div>
   )
