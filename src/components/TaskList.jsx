212fac79fb194884d5f35c9e2221ca2890190f26
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-22 23:19:01 -0500

    feat(styles): Giving good styles

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const TaskList = () => {
+  const [isDeleted, setIsDeleted] = useState(false)
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState({ title: '', description: '', completed: false });
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    completed: false,
+  });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -11,51 +16,61 @@ const TaskList = () => {
 
   const addTask = () => {
     setTasks([...tasks, newTask]);
-    setNewTask({ title: '', description: '', completed: false });
+    setNewTask({ title: "", description: "", completed: false });
   };
 
   const deleteTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
-  };
-
-  const toggleTaskCompletion = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index].completed = !updatedTasks[index].completed;
-    setTasks(updatedTasks);
+    setIsDeleted(true)
   };
 
   return (
-    <div>
-      <h2>Lista de Tareas</h2>
-      <input
-        type="text"
-        name="title"
-        placeholder="Título"
-        value={newTask.title}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="description"
-        placeholder="Descripción"
-        value={newTask.description}
-        onChange={handleInputChange}
-      />
-      <button onClick={addTask}>Agregar Tarea</button>
-      {tasks.map((task, index) => (
-        <div key={index}>
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-          <input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => toggleTaskCompletion(index)}
-          />
-          <button onClick={() => deleteTask(index)}>Eliminar</button>
-        </div>
-      ))}
+    <div className="contain">
+      <h2 className="text-center text-5xl font-bold my-6">Task list</h2>
+      <div className="w-[90%] mx-auto flex gap-x-4">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={newTask.title}
+          onChange={handleInputChange}
+          className="task-inp w-2/5 input-animation"
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={newTask.description}
+          onChange={handleInputChange}
+          className="task-inp w-2/5 input-animation"
+        />
+        <button
+          onClick={addTask}
+          className="text-5xl flex justify-center input-animation w-1/5 bg-teal-900 hover:bg-teal-700"
+        >
+          <p>+</p>
+        </button>
+      </div>
+      <div className="w-[90%] h-[70%] mb-10 p-4 grid grid-cols-3 mx-auto my-8 gap-8 overflow-y-scroll">
+        {tasks.map((task, index) => (
+          <div key={index} className="rounded-xl shadow-md shadow-teal-700 bg-slate-800" >
+            <h3 className="text-2xl text-center py-2 text-teal-400">
+              {task.title}
+            </h3>
+            <div className="mx-4 mb-4 mt-2 h-20 overflow-y-scroll flex justify-center items-center">
+              <p className="text-xl">{task.description}</p>
+            </div>
+            <button
+              onClick={() => deleteTask(index)}
+              className="bg-teal-900 hover:bg-teal-700 transition-colors duration-300 w-full h-14 rounded-b-xl"
+            >
+              Completed
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
